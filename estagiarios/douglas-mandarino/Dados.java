525e67e56ad52d872b8cb67ef37c7f3892c63ee2
Frederico <martinsporto@gmail.com>
2012-10-23 02:13:49 -0200

    Alteracoes no codigo do jogo de dados
    - Eliminacao do import de Math, pois pertence ao pacote lang que eh carregado por padrao
    - Mudei o nome da variavel array para dado. Pois ela representa o dado que sera lancado. Cada posicao do array seria uma das faces
    - Removi a variavel contador. Pois o proprio array "dado" informa quantas vezes cada face apareceu
    - Removi a variavel resultado e o scanner. Pois, o precisamos apenas saber quantas vezes cada face apareceu. Sem pedir valor ao usuario, a nao ser a quantidade de jogadas inicial
    - Mudei a formula de Math, pois nao estava sorteando para a primeira face (zero)
    - Mudei o nome da variavel num para face

--- estagiarios/douglas-mandarino/Dados.java
+++ estagiarios/douglas-mandarino/Dados.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Dados {
 
     public static void main(String[] args) {
 
-        int[] array = {0, 0, 0, 0, 0, 0};
-        int[] contador = {0, 0, 0, 0, 0, 0};
+        int[] dado = {0, 0, 0, 0, 0, 0};
 
         System.out.println("Quantas vezes jogará o dado?");
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int n = scan.nextInt();
+        int n = scanner.nextInt();
 
         for (int i = 0; i < n; i++) {
-            int num = (int) (1 + Math.random() * 5);
-            System.out.println("Qual o valor?");
-            Scanner scandado = new Scanner(System.in);
-            int resultado = scandado.nextInt();
-            contador[num] += 1;
-            array[num] += resultado;
+            int face = (int) (Math.random() * 6);
+            dado[face] += 1;
         }
 
         for (int i = 0; i < 6; i++) {
-            System.out.println("Indice " + i + " foi acessado:" + contador[i]);
+            System.out.println("Face " + (i + 1) + " foi sorteada: " + dado[i] + " vezes.");
         }
     }
 }
