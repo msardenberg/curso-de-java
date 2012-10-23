4bfe4ede0546b9ec277bb9f32d4ec900e3534e28
Marcelo Sardenberg <mf.sardenberg@gmail.com>
2012-10-23 16:08:26 -0200

    Update estagiarios/marcelo-sardenbergue/Fundamentos/parte1/Exercicio1.java

--- estagiarios/marcelo-sardenbergue/Fundamentos/parte1/Exercicio1.java
+++ estagiarios/marcelo-sardenbergue/Fundamentos/parte1/Exercicio1.java
@@ -1,10 +1,9 @@
-
 public class Exercicio1{
 
 	public static void main(String[] args) {
 
 		System.out.println("Marcelo Sardenberg deseja boas vindas!");
 		
- }
+ } 
 
-}
+} // agroa eu consigo interagir :D!
